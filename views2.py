e69094f96395db66713e87396f8dbc8e3e12306d
vikram3192 <146959727+vikram3192@users.noreply.github.com>
2024-04-18 12:13:38 +0530

    Add files via upload

---
+++ views2.py
@@ -0,0 +1,15 @@
+04.18 11:53 AM
+
+
+def Loginview(request):
+    if request.method == "POST":
+        username = request.POST['uname']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("show")
+        else:
+            return HttpResponse("invalid credentials")
